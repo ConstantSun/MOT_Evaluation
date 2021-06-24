820a2d6f3d6aba7d621c7b3b26e9c5ef30b7f9b0
Dương Hằng <duonghang0102@gmail.com>
2021-06-24 11:46:51 +0700

    Update eval.sh

--- MOT16_eval/eval.sh
+++ MOT16_eval/eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # run the evaluation
-python ~/Yolov5_DeepSort_Pytorch/MOT16_eval/TrackEval/scripts/run_mot_challenge.py --BENCHMARK MOT16 \
+python ~/MOT_Evaluation/MOT16_eval/TrackEval/scripts/run_mot_challenge.py --BENCHMARK MOT16 \
  --TRACKERS_TO_EVAL ch_yolov5m_deep_sort --SPLIT_TO_EVAL train --METRICS CLEAR Identity \
  --USE_PARALLEL False --NUM_PARALLEL_CORES 4
