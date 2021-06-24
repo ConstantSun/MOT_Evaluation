83df84e7292afeb9ddf69fcb9b9746818545289f
Dương Hằng <duonghang0102@gmail.com>
2021-06-24 12:52:06 +0700

    Update eval.sh

--- MOT16_eval/eval.sh
+++ MOT16_eval/eval.sh
@@ -3,4 +3,4 @@
 # run the evaluation
 python ~/MOT_Evaluation/MOT16_eval/TrackEval/scripts/run_mot_challenge.py --BENCHMARK MOT16 \
  --TRACKERS_TO_EVAL ch_yolov5m_deep_sort --SPLIT_TO_EVAL train --METRICS CLEAR Identity \
- --USE_PARALLEL False --NUM_PARALLEL_CORES 4
+ --USE_PARALLEL False --NUM_PARALLEL_CORES 1
